f4f6e5313305d5a3e082be6f20e150c9134ab882
christopinka <cmtopinka@gmail.com>
2024-12-09 12:46:59 -0600

    handle add/delete products

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "@/store/About.jsx";
+import Cart from "@/purchase/Cart.jsx";
+import Home from "@/store/Home.jsx";
+import Navbar from "@/ui/Navbar.jsx";
+import ProductDetailInfo from "@/product/ProductDetailInfo.jsx";
+import ProductDetailNutrition from "@/product/ProductDetailNutrition.jsx";
+import ProductDetailStorage from "@/product/ProductDetailStorage.jsx";
+import ProductDetails from "@/product/ProductDetails.jsx";
+import Products from "@/product/Products.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cart, setCart] = useState([]);
+  /* !! DO NOT REMOVE !!*/
+  window.__testsCart = cart; // used for the tests in this project
+
+  useEffect(() => {
+    // to visualize the cart in the console every time in changes
+    // you can also use React dev tools
+    console.log(cart);
+  }, [cart]);
+
+  function handleProductDelete(id) {
+    console.log("Deleting product " + id);
+    const updatedCart = cart.filter((product) => product.id !== id);
+      setCart(updatedCart);
+  }
+
+  function handleProductAdd(newProduct) {
+    // check if item exists
+    const existingProduct = cart.find(
+      (product) => product.id === newProduct.id
+    );
+
+    console.log(existingProduct);
+    if (existingProduct) {
+      // increase quantity
+      const updatedCart = cart.map((product) => {
+        if (product.id === newProduct.id) {
+          return {
+            ...product,
+            quantity: product.quantity + 1,
+          };
+        }
+        return product;
+      });
+      setCart(updatedCart);
+    } else {
+      // product is new to the cart
+      setCart([
+        ...cart,
+        {
+          ...newProduct,
+          quantity: 1,
+        },
+      ]);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <Navbar cart={cart} />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route
+            path="/products"
+            element={
+              <Products
+                cart={cart}
+                onProductAdd={handleProductAdd}
+                onProductDelete={handleProductDelete}
+            
+              />
+            }
+          ></Route>
+          <Route
+            path="/products/:id/"
+            element={<ProductDetails onProductAdd={handleProductAdd} />}
+          >
+            <Route
+              path=""
+              element={<ProductDetailInfo onProductAdd={handleProductAdd} />}
+            ></Route>
+
+            <Route
+              path="nutrition"
+              element={<ProductDetailNutrition />}>
+            </Route>
+
+            <Route path="storage" element={<ProductDetailStorage />}></Route>
+          </Route>
+          <Route path="cart" element={<Cart cart={cart} />}></Route>
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/product/Product.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import Button from "@/ui/Button.jsx";
+
+export default function Product(props) {
+  const { details } = props;
+
+  return (
+    <div className="product">
+      <div className="product-image-container">
+        <Link to={`/products/${details.id}`}>
+          <img
+            src={details.image}
+            width="100"
+            height="100"
+            className="product-image"
+            alt={details.name}
+          />
+        </Link>
+          <div className="product-quantity-container">
+            <div className="product-quantity">0</div>
+          </div>
+      </div>
+      <div className="product-info">
+        <h3>{details.name}</h3>
+        <p>{details.description}</p>
+      </div>
+      <div className="product-checkout">
+        <div>
+            <Button
+              outline
+              onClick={() => props.onProductDelete(details.id)}
+              className="product-delete"
+            >x</Button>
+        </div>
+        <Button outline onClick={() => props.onProductAdd(details)}>
+          ${details.price}
+        </Button>
+      </div>
+    </div>
+  );
+}
