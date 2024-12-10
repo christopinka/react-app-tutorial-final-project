e46668d5521ead6f29c5ad23f82b7476e1626fae
christopinka <cmtopinka@gmail.com>
2024-12-10 11:13:49 -0600

    stripe payment

--- src/purchase/Cart.jsx
+++ src/purchase/Cart.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import Input from "@/ui/Input.jsx";
+import Button from "@/ui/Button.jsx";
 
+const stripeLoadedPromise = loadStripe(
+  "pk_test_51HsqkCGuhXEITAut89vmc4jtjYd7XPs8hWfo2XPef15MFqI8rCFc8NqQU9WutlUBsd8kmNqHBeEmSrdMMpeEEyfT00KzeVdate"
+);
 
 export default function Cart({ cart }) {
   const totalPrice = cart.reduce(
     (total, product) => total + product.price * product.quantity,
     0
   );
+
+  const [email, setEmail] = useState("");
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const lineItems = cart.map((product) => {
+      return { price: product.price_id, quantity: product.quantity };
+    });
+
+    stripeLoadedPromise.then((stripe) => {
+      stripe
+        .redirectToCheckout({
+          lineItems: lineItems,
+          mode: "payment",
+          successUrl: "https://react-tutorial.app/app.html",
+          cancelUrl: "https://react-tutorial.app/app.html",
+          customerEmail: email,
+        })
+        .then((response) => {
+          // this will only log if the redirect did not work
+          console.log(response.error);
+        })
+        .catch((error) => {
+          // wrong API key? you will see the error message here
+          console.log(error);
+        });
+    });
+  }
 
   return (
     <div className="cart-layout">
@@ -56,6 +92,20 @@ export default function Cart({ cart }) {
                 </tr>
               </tfoot>
             </table>
+            <form className="pay-form" onSubmit={handleFormSubmit}>
+              <p>
+                Enter your email and then click on pay and your products will be
+                delivered to you on the same day!
+              </p>
+              <Input
+                placeholder="Email"
+                onChange={(event) => setEmail(event.target.value)}
+                value={email}
+                type="email"
+                required
+              />
+              <Button type="submit">Pay</Button>
+            </form>
           </>
         )}
       </div>
